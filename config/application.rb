2acd06e51ac440b0737968c17ce5f0ec38d29a09
Bryan McKelvey <bryan.mckelvey@gmail.com>
2012-03-12 16:34:29 +0900

    Don't initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module ReasonableSite
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku requires this
+    config.assets.initialize_on_precompile = false
   end
 end
